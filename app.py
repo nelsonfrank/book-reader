4b3f7a50ca4dc41ba07559dc4139d8a67e5cdf42
nelson frank <nelsonfr78@outlook.com>
2022-09-24 15:57:38 +0300

    feat: Static files and template inheritance

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world"
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
